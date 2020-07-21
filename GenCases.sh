3104be356dd9f1e42aea23ef9bfb22df17765083
Mohamed Gamal Tawfik <mohamedjooo75@gmail.com>
2020-07-21 11:08:35 +0200

    Found a bug in Add circuit

--- GenCases.sh
+++ GenCases.sh
@@ -1,27 +1,28 @@
 rm TestCases 
 > TestCases
 
+
 for (( i = 0; i < 100; i++ ))
 do
-echo " $RANDOM.$RANDOM * $RANDOM.$RANDOM;" >> TestCases
+echo " $RANDOM.$RANDOM - $RANDOM.$RANDOM;" >> TestCases
 done
 
 for (( i = 0; i < 100; i++ ))
 do
-echo " 0.$RANDOM * $RANDOM.$RANDOM;" >> TestCases
+echo " 0.$RANDOM - $RANDOM.$RANDOM;" >> TestCases
 done
 
 for (( i = 0; i < 100; i++ ))
 do
-echo " 0.00$RANDOM * 0.$RANDOM;" >> TestCases
+echo " 0.00$RANDOM - 0.$RANDOM;" >> TestCases
 done
 
 for (( i = 0; i < 100; i++ ))
 do
-echo " $RANDOM$RANDOM.$RANDOM * 0.$RANDOM;" >> TestCases
+echo " $RANDOM$RANDOM.$RANDOM - 0.$RANDOM;" >> TestCases
 done
 
 for (( i = 0; i < 100; i++ ))
 do
-echo " $RANDOM$RANDOM.$RANDOM * $RANDOM$RANDOM.$RANDOM;" >> TestCases
+echo " $RANDOM$RANDOM.$RANDOM - $RANDOM$RANDOM.$RANDOM;" >> TestCases
 done
